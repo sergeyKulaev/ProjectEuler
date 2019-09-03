a5181da38557aafacad3e177aab8df6845d7f998
Sergey Kulaev <kulaevsergey2014@gmail.com>
2019-09-03 16:43:32 +0300

    Solve Problem 1

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ Multiples of 3 and 5
+ If we list all the natural numbers below 10 that 
+ are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ The sum of these multiples is 23.
+ Find the sum of all the multiples of 3 or 5 below 1000.
+*/
 namespace Problem1
 {
     class Program
     {
+        static int FindSumOfMultiplies(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+            return sum;
+        }
         static void Main(string[] args)
         {
+            FindSumOfMultiplies(1000);
         }
     }
 }
